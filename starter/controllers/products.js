826a1fd8a6c67c6aa7876ec32a28aad2d379d26a
Samad-Azeez <azeezsamad2004@gmail.com>
2024-09-09 22:03:08 +0100

    Update product controller to fetch products dynamically and statically

--- starter/controllers/products.js
+++ starter/controllers/products.js
@@ -1,10 +1,15 @@
+import {product_model} from "../models/Product.js";
+
+// Get all products with the featured field set to true stically
 export const getAllProductsStatic = async (req, res) => {
-    throw new Error("Testing static error handling");
-    // This line won't be executed due to the thrown error
-    res.status(200).json({ message: "Products testing route" });
+    const products = await product_model.find({featured: true}); // Get all products with the featured field set to true
+    const nbHits = products.length; // Get the number of products returned
+    res.status(200).json({ products, nbHits }); // Send the products and the number of hits
   };
   
-
+// get all products dynamically
 export const getAllProducts = async (req, res) => {
-    res.status(200).json({ message: "Products route" });
+    const products = await product_model.find(req.query); // Get all products
+    const nbHits = products.length; // Get the number of products returned
+    res.status(200).json( { products, nbHits } );
 };
--- starter/populate.js
+++ starter/populate.js
@@ -1,6 +1,6 @@
 import 'dotenv/config'; // Load environment variables from .env into process.env
 import { connectDB } from './db/connect.js'; 
-import { product_model } from './models/product.js'; // Import Product model
+import { product_model } from './models/pproduct.sjs'; // Import Product model
 import jsonProducts from './products.json' assert { type: 'json' }; // Import products data from JSON file
 
 // Asynchronous function to populate the database with products
@@ -12,7 +12,7 @@ const start = async () => {
         await product_model.deleteMany(); // Delete existing products
         console.log('Existing products deleted...');
     
-        await product_model.create(jsonProducts); // Insert new products
+        await product_model.create(jsonProducts, { runValidators: true }); // Insert new products
         console.log('New products inserted...');
 
         process.exit(0); // Exit on success
