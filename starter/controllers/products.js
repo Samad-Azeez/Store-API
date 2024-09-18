5871b07a4292f5b11afcfc3a762699422b98e1bf
Samad-Azeez <azeezsamad2004@gmail.com>
2024-09-18 14:13:22 +0100

    Refactor product controller to fetch products dynamically and statically

--- starter/controllers/products.js
+++ starter/controllers/products.js
@@ -1,38 +1,38 @@
-import {product_model} from "../models/Product.js";
+import { product_model } from '../models/product.js';
 
 // Get all products with the featured field set to true stically
 export const getAllProductsStatic = async (req, res) => {
-    const products = await product_model.find({featured: false}); // Get all products with the featured field set to true
-    const nbHits = products.length; // Get the number of products returned
-    res.status(200).json({ products, nbHits }); // Send the products and the number of hits
-  };
-  
+  const products = await product_model.find({ featured: false }); // Get all products with the featured field set to true
+  const nbHits = products.length; // Get the number of products returned
+  res.status(200).json({ products, nbHits }); // Send the products and the number of hits
+};
+
 // get all products dynamically
 export const getAllProducts = async (req, res) => {
-    const { featured, company, name } = req.query;
-    const queryObject = {};
-    
-    // If the featured query parameter is provided, add it to the query object
-    if (featured === 'true') { 
-        queryObject.featured = true;
-      } else if (featured === 'false') {
-        queryObject.featured = false;
-    }
+  const { featured, company, name } = req.query;
+  const queryObject = {};
 
-    // If the company query parameter is provided, add it to the query object
-    if (company) {
-        queryObject.company = company;
-    }
+  // If the featured query parameter is provided, add it to the query object
+  if (featured === 'true') {
+    queryObject.featured = true;
+  } else if (featured === 'false') {
+    queryObject.featured = false;
+  }
 
-    // If the name query parameter is provided, add it to the query object
-    if (name) {
-        queryObject.name = { $regex: name, $options: 'i' };
-    }
+  // If the company query parameter is provided, add it to the query object
+  if (company) {
+    queryObject.company = company;
+  }
 
-    // Log the query object to the console
-    console.log(queryObject);
+  // If the name query parameter is provided, add it to the query object
+  if (name) {
+    queryObject.name = { $regex: name, $options: 'i' };
+  }
 
-    const products = await product_model.find( queryObject ); // Get all products
-    const nbHits = products.length; // Get the number of products returned
-    res.status(200).json( { products, nbHits } );
+  // Log the query object to the console
+  console.log(queryObject);
+
+  const products = await product_model.find(queryObject); // Get all products
+  const nbHits = products.length; // Get the number of products returned
+  res.status(200).json({ products, nbHits });
 };
