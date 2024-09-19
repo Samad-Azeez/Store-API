d27fdb71845c6f254de8a957d4a0b9e6ba4385c7
Samad-Azeez <azeezsamad2004@gmail.com>
2024-09-19 18:48:32 +0100

    Added dynamic sorting and field selection for products query

--- starter/controllers/products.js
+++ starter/controllers/products.js
@@ -2,14 +2,14 @@ import { product_model } from '../models/product.js';
 
 // Get all products with the featured field set to true stically
 export const getAllProductsStatic = async (req, res) => {
-  const products = await product_model.find({}).sort('-name price'); // Get all products with the featured field set to true
+  const products = await product_model.find({}).select('name price'); // Get all products with the featured field set to true
   const nbHits = products.length; // Get the number of products returned
   res.status(200).json({ products, nbHits }); // Send the products and the number of hits
 };
 
 // get all products dynamically
 export const getAllProducts = async (req, res) => {
-  const { featured, company, name, sort } = req.query;
+  const { featured, company, name, sort, fields } = req.query;
   const queryObject = {};
 
   // If the featured query parameter is provided, add it to the query object
@@ -40,6 +40,12 @@ export const getAllProducts = async (req, res) => {
     result = result.sort(sortList);
   } else {
     result = result.sort('createdAt'); // Sort by creation date if no sort query parameter is provided
+  }
+
+  // If the fields query parameter is provided, select only the specified fields
+  if (fields) {
+    const fieldsList = fields.split(',').join(' '); // Handle comma-separated fields like 'name,price'
+    result = result.select(fieldsList); // Select only the specified fields
   }
 
   const products = await result; //Execute the query and await the result
