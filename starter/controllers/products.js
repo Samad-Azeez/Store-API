392dec39091e4c421e87f7676c8a689d94e55eed
Samad-Azeez <azeezsamad2004@gmail.com>
2024-09-19 14:35:20 +0100

    Refactor product controller to fetch products dynamically and statically

--- starter/controllers/products.js
+++ starter/controllers/products.js
@@ -2,14 +2,14 @@ import { product_model } from '../models/product.js';
 
 // Get all products with the featured field set to true stically
 export const getAllProductsStatic = async (req, res) => {
-  const products = await product_model.find({ featured: false }); // Get all products with the featured field set to true
+  const products = await product_model.find({}).sort('-name price'); // Get all products with the featured field set to true
   const nbHits = products.length; // Get the number of products returned
   res.status(200).json({ products, nbHits }); // Send the products and the number of hits
 };
 
 // get all products dynamically
 export const getAllProducts = async (req, res) => {
-  const { featured, company, name } = req.query;
+  const { featured, company, name, sort } = req.query;
   const queryObject = {};
 
   // If the featured query parameter is provided, add it to the query object
@@ -32,7 +32,13 @@ export const getAllProducts = async (req, res) => {
   // Log the query object to the console
   console.log(queryObject);
 
-  const products = await product_model.find(queryObject); // Get all products
+  // If the sort query parameter is provided, sort the products accordingly
+  let result = product_model.find(queryObject); // Get all products
+  if (sort) {
+    products = products.sort();
+  }
+  const products = await result;
+
   const nbHits = products.length; // Get the number of products returned
   res.status(200).json({ products, nbHits });
 };
